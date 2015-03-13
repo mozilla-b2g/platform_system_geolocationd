a9807631ee6f08bf8e51405b65847bc4912d6967
Thomas Zimmermann <tdz@users.sourceforge.net>
2015-03-13 14:33:41 +0100

    Bug 1107539: Added main functions and initialization

--- src/Android.mk
+++ src/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= io.c \
+                  main.c \
                   pdu.c \
                   registry.c \
                   service.c
@@ -9,6 +10,8 @@ LOCAL_C_INCLUDES := system/libfdio/include \
                     system/libpdu/include
 LOCAL_CFLAGS := -DANDROID_VERSION=$(PLATFORM_SDK_VERSION) -Wall -Werror
 LOCAL_SHARED_LIBRARIES := libpdu \
+                          libfdio \
+                          libhardware_legacy \
                           liblog
 LOCAL_MODULE:= geolocationd
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
